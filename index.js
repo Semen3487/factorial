baccc99d2de7168a2eb674394084344297a986e2
Sam <sopenday@gmail.com>
2022-04-14 21:06:15 +0300

    Add initial commit

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+// function fact(n){
+//   if(n != 0){
+//       return n * fact(n - 1);
+//   } else{
+//       return 1;
+//   }
+// }
+// console.log(fact(4));
+
 function fact(n){
-  if(n != 0){
+  if((n < 0) || (n % 1 !== 0)){
+      return 'The number is incorrect'; 
+  } else if(n !== 0){
       return n * fact(n - 1);
-  } else{
-      return 1;
   }
+  return 1;  
 }
 console.log(fact(4));
